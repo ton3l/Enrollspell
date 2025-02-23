eb2abb7558731d35e9897626d8fc97e25c45f139
Elton <elton.felype.s.nascimento@gmail.com>
2025-02-23 16:05:14 -0300

    Pequena correção em login.php

--- src/API/login.php
+++ src/API/login.php
@@ -6,13 +6,15 @@
     $matricula = $_POST['matricula'];
     $senha = $_POST['senha'];
     
-    $stmt = $DAL->query("SELECT matricula, senha FROM aluno WHERE matricula = $matricula");
+    $stmt = $DAL->query("SELECT matricula, senha FROM aluno WHERE matricula = '$matricula'");
     $verify = $stmt->fetch();
-
+    
     if(isset($verify['matricula']) && $matricula == $verify['matricula'] && $senha == $verify['senha']){
         header("Location: http://127.0.0.1/Enrollspell/src/views/studentSpace.php?matricula=$matricula"); 
         exit();
     }
-
-    //if();
+    else{
+        header("Location: http://127.0.0.1/Enrollspell/src/views/login.html"); 
+        exit();
+    }
 ?>
